3fa8e26b479b937c7b369c72d9424618c2d8ff83
Shuxiong Ye <yeshuxiong@gmail.com>
2016-06-12 11:30:22 -0700

    Fix bug.

--- initialize/software/software.sh
+++ initialize/software/software.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-sudo apt-get install -y chromium-browser eclipse tmux subversion maven2
-
-
-sudo apt-get install chromium-browser ttf-wqy-zenhei flashplugin-installer -y
+sudo apt-get install -y chromium-browser eclipse tmux subversion maven \
+    ttf-wqy-zenhei flashplugin-installer
